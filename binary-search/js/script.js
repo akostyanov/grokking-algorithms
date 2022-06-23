21787e1dfd84f9ad1e0713d34de9715edfaabdec
akostyanov <akostianov@yandex.ru>
2022-06-23 10:21:28 +0300

    feat: update script

--- binary-search/js/script.js
+++ binary-search/js/script.js
@@ -1,10 +1,10 @@
 
 function binarySearch(list, item) {
     let low = 0;
-    let high = list.lenght - 1;
+    let high = list.length - 1;
 
     while (low <= high) {
-        let mid = (low + high) / 2;
+        let mid = Math.floor((low + high) / 2);
         let guess = list[mid];
         if (guess == item) { return mid; }
         else if (guess > item) { high = mid + 1; }
@@ -17,5 +17,5 @@ function binarySearch(list, item) {
 
 
 let myList = [1, 3, 5, 7, 9];
-console.log(myList.lenght);
-console.log(binarySearch(myList, 5));
+console.log(myList.length);
+console.log(binarySearch(myList, 1));
